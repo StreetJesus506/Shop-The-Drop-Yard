5eff5d771c9c62ab05da7117715f7dcde32af235
StreetJesus506 <75284845+StreetJesus506@users.noreply.github.com>
2026-07-29 15:12:56 -0600

    Add image domains to Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'images-api.printify.com',
+      'storage.googleapis.com',
+    ],
+  },
+}
+
+module.exports = nextConfig
