03b85ac233b63754ad2f7ff1d007c631643fa80b
StreetJesus506 <75284845+StreetJesus506@users.noreply.github.com>
2026-07-30 21:41:06 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -136,10 +136,11 @@ function ShippingContainer({ brand, isActive, onClick }) {
     line.position.set(0, 0, 0.54)
     group.add(line)
 
-    group.rotation.y = 0.5
-group.rotation.x = -0.08
-group.position.x = -1.8
+    group.rotation.y = 0.3
+group.rotation.x = -0.06
+group.position.x = -2.4
 group.scale.set(1.4, 1.4, 1.4)
+
 
 
 
