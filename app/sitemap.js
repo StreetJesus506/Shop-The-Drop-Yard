a1a36d6ccc1583ca48759c0aa6bc7b0ba6d51991
StreetJesus506 <75284845+StreetJesus506@users.noreply.github.com>
2026-09-02 13:34:01 -0600

    Add 'about/streetjesus' to sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -29,6 +29,13 @@ export default function sitemap() {
       changeFrequency: 'monthly',
       priority: 0.5,
     },
+    {
+  url: `${baseUrl}/about/streetjesus`,
+  lastModified: new Date(),
+  changeFrequency: 'monthly',
+  priority: 0.7,
+},
+
     ...brandPages,
   ]
 }
