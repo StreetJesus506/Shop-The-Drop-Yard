1155a9547dddda01e75850958dddcc309b75fbb5
StreetJesus506 <75284845+StreetJesus506@users.noreply.github.com>
2026-09-21 19:42:14 -0600

    Update organization logo and Instagram link

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+
+
 import './globals.css'
 import { CartProvider } from '@/lib/cartContext'
 import Cart from '@/components/Cart'
@@ -12,7 +14,7 @@ export const metadata = {
     url: 'https://shopthedropyard.com',
     siteName: 'The Drop Yard',
     type: 'website',
-  }
+  },
 }
 
 const organizationSchema = {
@@ -20,14 +22,14 @@ const organizationSchema = {
   '@type': 'Organization',
   name: 'The Drop Yard',
   url: 'https://shopthedropyard.com',
-  logo: 'https://shopthedropyard.com',
+  logo: 'https://shopthedropyard.com/logos/Logo-Red.png',
   contactPoint: {
     '@type': 'ContactPoint',
     email: 'contact@shopthedropyard.com',
     contactType: 'customer service',
   },
   sameAs: [
-    'https://instagram.com',
+    'https://www.instagram.com/streetjesusgotsoul',
   ],
 }
 
@@ -46,11 +48,6 @@ const storeSchema = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://googleapis.com" />
-        <link rel="preconnect" href="https://gstatic.com" crossOrigin="anonymous" />
-        <link href="https://googleapis.com/css2?family=Big+Shoulders+Stencil:wght@900&family=Space+Mono&family=Work+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
-      </head>
       <body>
         <script
           type="application/ld+json"
