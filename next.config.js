804e6911e14f6f8fa01921160f6d7db584fb7234
StreetJesus506 <75284845+StreetJesus506@users.noreply.github.com>
2026-09-22 14:30:37 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
   images: {
     domains: [
       'images-api.printify.com',
