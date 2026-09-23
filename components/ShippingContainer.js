67f1d4b6581aa7d643f01f4ad48218b57f4c5787
StreetJesus506 <75284845+StreetJesus506@users.noreply.github.com>
2026-09-23 07:43:08 -0600

    Adjust camera position in ShippingContainer.js

--- components/ShippingContainer.js
+++ components/ShippingContainer.js
@@ -16,7 +16,7 @@ export default function ShippingContainer({ brand, isActive, onClick }) {
 
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(50, w / h, 0.1, 100)
-    camera.position.set(-2.8, 0.3, 5)
+    camera.position.set(-3.4, 0.3, 5)
     camera.lookAt(-0.6, 0, 0)
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, preserveDrawingBuffer: true })
