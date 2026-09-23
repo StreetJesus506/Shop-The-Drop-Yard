e2430e5ca7b5bbf27c2b0cf3005434455aac2827
StreetJesus506 <75284845+StreetJesus506@users.noreply.github.com>
2026-09-23 07:08:58 -0600

    Update ShippingContainer.js

--- components/ShippingContainer.js
+++ components/ShippingContainer.js
@@ -16,8 +16,8 @@ export default function ShippingContainer({ brand, isActive, onClick }) {
 
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(50, w / h, 0.1, 100)
-    camera.position.set(-2.8, 0.3, 5.6)
-    camera.lookAt(0, 0, 0)
+    camera.position.set(-2.8, 0.3, 5)
+    camera.lookAt(-0.6, 0, 0)
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, preserveDrawingBuffer: true })
     renderer.setSize(w, h)
